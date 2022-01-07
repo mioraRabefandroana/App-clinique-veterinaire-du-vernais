4532265e7bd5f22ecd2a3941fbae5b5a97cf0511
Houssainatou <bhoussainatou96@gmail.com>
2022-01-07 05:17:06 +0100

    Resolution de conflit: login

--- App/src/App.svelte
+++ App/src/App.svelte
@@ -1,7 +1,68 @@
 <script>
     import Home from './Home/Home.svelte'
+    import { Router, Link, link, Route } from "svelte-routing";
+
+    import FooterNav from './Common/FooterNav.svelte';
+
+    import Map from './Infos/Map.svelte';
+    import Hours from './Infos/Hours.svelte';
+    import Contact from './Infos/Contact.svelte';
+    
+    import { currentUser } from './store';
+import Appointment from './Appointment/Appointment.svelte';
+import AnimalList from './Animal/AnimalList.svelte';
+import NewAnimal from './Animal/NewAnimal.svelte';
+import EditAnimal from './Animal/EditAnimal.svelte';
+
+import Remote from './Service/Remote.svelte';
+import Care from './Service/Care.svelte';
+import Vaccination from './Service/Vaccination.svelte';
+import Advice from './Service/Advice.svelte';
+import MyAppointment from './Appointment/MyAppointment.svelte';
+
+import Infos from './Infos/Infos.svelte';
+import Login from './Connexion/Login.svelte';
+import HeaderNav from './Common/HeaderNav.svelte';
+import Register from './Connexion/Register.svelte';
+    
+    let url = "";
+
+    export function getCurrentUser(){
+        // return null; // DEBUG
+
+        if("id" in $currentUser && $currentUser["id"])
+            return currentUser;
+        return null;
+    }
 </script>
 
-<link rel="stylesheet" href="global.css">
+<HeaderNav user={ getCurrentUser() } />
 
-<Home/>
+
+<Router url="{url}">
+    <Route path="/" component="{Home}" />
+    <Route path="contact" component="{Contact}" />
+    <Route path="map" component="{Map}" />
+    <Route path="hour" component="{Hours}" />
+    <Route path="appointment/:type" component="{Appointment}" let:params >
+        <BlogPost type="{params.type}" />
+    </Route>
+    <Route path="myappointment" component="{MyAppointment}" />
+    <Route path="animal" component="{AnimalList}"/>
+    <Route path="animal/new" component="{NewAnimal}"/>
+    <Route path="animal/edit" component="{EditAnimal}"/>
+    <Route path="infos" component="{Infos}" />
+
+
+    <Route path="remote" component="{Remote}" />
+    <Route path="care" component="{Care}" />
+    <Route path="vaccination" component="{Vaccination}" />
+    <Route path="advice" component="{Advice}" />
+
+    <Route path="login" component="{Login}" />
+    <Route path="register" component="{Register}"/>
+
+</Router>
+    
+
+<FooterNav user={ getCurrentUser() }/>
--- App/src/Connexion/Login.svelte
+++ App/src/Connexion/Login.svelte
@@ -1,1 +1,69 @@
-<div>LOGIN</div>
+<script>
+    import { useForm, validators, Hint, required } from "svelte-use-form";
+    const form = useForm();
+ </script>
+
+ <div class="form-content">
+   <div class="form-header">Connexion</div>
+   <form use:form>
+       <div>
+           <span><i class="fas fa-user"></i></span> <input type="text" name="pseudo" placeholder="Identifiant" use:validators={[required]} />
+           <Hint for="pseudo" on="required">L'identifiant est obligatoire</Hint>
+       </div>
+       <div>
+           <pan><i class="fas fa-lock"></i></pan> <input type="password" name="password" placeholder="Mot de passe" use:validators={[required]} />
+           <Hint for="password" on="required">Mot de passe obligatoire</Hint>
+       </div>
+       <div>
+           <button disabled={!$form.valid}>Se connecter</button>
+       </div>
+   </form>
+   <a href="#" >S'inscire</a>
+</div>
+
+ <style>
+     :global(.touched:invalid) {
+         border-color: red;
+         outline-color: red;
+     }
+     button{
+       color: white;
+       padding: 0.5rem 0;
+       margin-top: 0.5rem;
+       display: inline-block;
+       width: 100%;
+       border-radius: 0.25rem;
+       cursor:pointer; 
+       background-color: var(--c_blue);
+     }
+     :disabled{
+         background-color: var(--c_gray);
+     }
+     .form-content{
+       background-color: #fff;
+       border-radius: 10px;
+       box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1), 0 6px 6px rgba(0, 0, 0, 0.1);
+       padding: 50px 20px;
+       text-align: center;
+       max-width: 100%;
+       width: 350px;    
+     }
+     .form-header{
+       color: var(--c_blue);
+       align-content: center;
+       text-align: center;
+     }
+     input[type=text],input[type=password]{
+       padding: 0.5rem 0;
+       margin-top: 0.5rem;
+       border-width: 1px;
+       border-radius: 0.25rem;
+   }
+     div{
+         text-align: center;
+         font-size: x-large;
+     }
+    a{
+        text-align: right;
+    }
+ </style>
